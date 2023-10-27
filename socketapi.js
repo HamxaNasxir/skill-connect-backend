aaa519dfe7ee80d9e69a1930399b84c3cda56863
awais-shahbaz-tech <awaishahbaz@gmail.com>
2023-10-27 14:38:46 +0500

    Socket APi

---
+++ socketapi.js
@@ -0,0 +1,42 @@
+let io;
+let activeUsers = [];
+let socketServer = function (server) {
+  io = require("socket.io")(server, { cors: { origin: "*" } });
+  io.on("connection", function (server) {
+    // Add new user
+    socket.on("new-user-add", (newUserId) => {
+      //if user is not added previously
+      if (!activeUsers.some((user) => user.userId === newUserId)) {
+        activeUsers.push({ userId: newUserId, socketId: socket.id });
+        console.log("New User Connected", activeUsers);
+      }
+      io.emit("get-users", activeUsers);
+    });
+    // disconnect
+    socket.on("disconnect", () => {
+      activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+      console.log("user Disconnected", activeUsers);
+
+      io.emit("get-users", activeUsers);
+    });
+    // send-message
+    socket.on("send-message", (data) => {
+      const { receiverId } = data;
+      const user = activeUsers.finf((user) => user.userId === receiverId);
+      if (user) {
+        io.to(user.socketId).emit("recieve-message", data);
+      }
+    });
+  });
+};
+
+let Socket = function () {
+  return {
+    emit: function (event, data) {
+      io.sockets.emit(event, data);
+    },
+  };
+};
+
+exports.socketServer = socketServer;
+exports.Socket = Socket;
