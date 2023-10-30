24281b2984dce303e41d0fbef10358575e292d2a
awais-shahbaz-tech <awaishahbaz@gmail.com>
2023-10-30 17:47:32 +0500

    Find Jobs Api

--- job/controller.js
+++ job/controller.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const Job = require("./model");
 
+//  @desc   :  Create Job
+//  @Route  :  POST /jobs
+//  @access :  Public
 const createJob = asyncHandler(async (req, res) => {
   try {
     const jobs = await Job.create({ ...req.body });
@@ -15,25 +18,31 @@ const createJob = asyncHandler(async (req, res) => {
   }
 });
 
+//  @desc   :  Get Job
+//  @Route  :  GET /jobs/:id
+//  @access :  Public
 const getJobById = asyncHandler(async (req, res) => {
   try {
     const id = req.params.id;
     const jobs = await Job.findById(id).exec();
 
-    if(!jobs){
+    if (!jobs) {
       res.status(500).json("Job not found.");
     }
-    
+
     res.status(200).json(jobs);
   } catch (error) {
-    res.status(500).json(error.message)
+    res.status(500).json(error.message);
   }
 });
 
+//  @desc   :  Get Job By UserId
+//  @Route  :  GET /jobs/user/:id
+//  @access :  Public
 const getJobByUserID = asyncHandler(async (req, res) => {
   try {
     const id = req.params.id;
-    const jobs = await Job.find({ userId: id }).sort({createdAt:-1}).exec();
+    const jobs = await Job.find({ userId: id }).sort({ createdAt: -1 }).exec();
 
     if (!jobs) {
       res.status(500).json("No jobs found.");
@@ -42,12 +51,39 @@ const getJobByUserID = asyncHandler(async (req, res) => {
 
     res.status(200).json(jobs);
   } catch (error) {
-    console.log(error.message);
+    res.status(500).json(error.message);
   }
 });
+
+//  @desc   :  Update Job
+//  @Route  :  PUT /jobs/:id
+//  @access :  Public
+const updateJob = asyncHandler(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const jobs = await Job.updateOne(
+      { _id: id },
+      { $set: { ...req.body } },
+      { new: true }
+    );
+
+    if (!jobs) {
+      res.status(400);
+      throw new Error("Job not found.");
+    }
+    res.status(200).json("Job has been updated.");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//  @desc   :  Delete Job
+//  @Route  :  DELETE /jobs/:id
+//  @access :  Public
 
 module.exports = {
   createJob,
   getJobById,
   getJobByUserID,
+  updateJob,
 };
--- job/index.js
+++ job/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const routes = express.Router();
 const controller = require("./controller");
 
-const { createJob, getJobById, getJobByUserID } = controller;
+const { createJob, getJobById, getJobByUserID, updateJob } = controller;
 
 routes.post("/", createJob);
-routes.get("/:id", getJobById);
+routes.route("/:id").get(getJobById).put(updateJob);
 routes.get("/user/:id", getJobByUserID);
 
 module.exports = routes;
